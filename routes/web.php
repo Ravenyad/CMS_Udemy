4cd994bbc4e782f911a4d20f65986209db45125c
Ervan Haryadi <erhar23@gmail.com>
2021-03-25 00:55:24 +0700

    [asgmt2] creating categories list database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categories', [CategoriesController::class, 'cat_index']
+)->middleware(['auth'])->name('categories');
+
+Route::get('/categories/new', [CategoriesController::class, 'cat_new']
+)->middleware(['auth'])->name('/categories/new');
+
+Route::post('/categories/create_cat', [CategoriesController::class, 'cat_create']
+)->middleware(['auth'])->name('/categories/create');
+
+Route::get('/categories/{category}/edit', [CategoriesController::class, 'cat_edit']
+)->middleware(['auth'])->name('/categories/{category}/edit');
+
+Route::post('/categories/{category}/updated', [CategoriesController::class, 'cat_upd']
+)->middleware(['auth'])->name('/categories/{category}/updated');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
